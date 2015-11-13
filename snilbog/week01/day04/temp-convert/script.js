cc05db75c50941a58bfe4709bf8554fa3c82cab0
snilbog <fitch.neal@gmail.com>
2015-11-13 10:14:04 -0800

    more tweeks and added class notes

--- snilbog/week01/day04/temp-convert/script.js
+++ snilbog/week01/day04/temp-convert/script.js
@@ -1,6 +1,9 @@
 script.js
 
- var myInput = document.querySelector("input[type='text']");
+
+
+/*
+var myInput = document.querySelector("input[type='text']");
 
 var myButton = document.querySelector('button');
 
@@ -15,4 +18,30 @@ function onClick(){
   var myCelcius = document.querySelector('.celcius');
   myCelcius.innerText = c + " \u2103";
   }
-}
+} */
+
+
+
+***********Class Notes********************
+
+var convert = function(fTemp) {
+	return (fTemp = 32) * (5/9);
+	};
+
+	document.addEventListener('DOMContentLoaded'), function() {
+	var inputField = document.getElementById('temperature');
+	var celsiusDiv = document.getElementById('convertedTemp');
+
+	document.getElementById('clear').addEventLlistener('click', function(e) {
+		inputField.value = '';
+		celsiusDiv.innerHTML;
+
+	});
+	
+	document.getElementById('temperatureForm').
+	addEventListener('submit', function(e) {
+	e.preventDefault();
+	celsiusDiv.innerHTML = convert(inputField.value) + "&deg;";
+
+	});
+});    
